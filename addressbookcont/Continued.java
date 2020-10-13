7252bac75074cf588ad4d5a59cd2ac9812cd86d9
RUCHIT680 <ruchitrai282@gmail.com>
2020-10-13 23:46:02 +0530

    add

---
+++ addressbookcont/Continued.java
@@ -0,0 +1,27 @@
+package addressbookcont;
+import java.io.*;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import AddressBook.Collection;
+import com.bridgelabz.info;
+public class Continued {
+	private HashMap<String, ArrayList<info>> deathNote;
+	public static void main(String[] args) {
+		System.out.println("1. Add a new contact.");
+		System.out.println("2. Edit an existing contact.");
+		System.out.println("3. Delete an existing contact.");
+
+
+		user_input = stdlin.next();
+		
+	}
+
+List<info> noDuplicates = friends.stream().distinct().collect(Collectors.toList());
+      System.out.println("List with duplicates removed:" + noDuplicates);
+
+	
+
+} 
+
