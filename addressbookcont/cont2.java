179ce66f3294e91a490b807f037e6f24242ffad8
RUCHIT680 <ruchitrai282@gmail.com>
2020-10-13 23:59:12 +0530

    added

---
+++ addressbookcont/cont2.java
@@ -0,0 +1,23 @@
+package addressbookcont;
+
+public class cont2 {
+	public String fName; 
+	public String lName;
+	public String address;
+
+	public String phone;
+	public String email;
+
+	public info(String firstName, String lastName, String address, String city, String state, String zip,
+	public Info(String firstName, String lastName, String address, String city, String state, String zip,
+			String phoneNo, String email) {
+		this.fName = firstName; 
+		this.lName = lastName;
+		
+		this.phone = phoneNo;
+		this.email = email;
+	}
+
+
+} 
+
