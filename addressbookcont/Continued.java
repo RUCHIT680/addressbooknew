d82a85eda3155c73ae8a731be9d55599d41aa985
RUCHIT680 <ruchitrai282@gmail.com>
2020-10-14 00:17:49 +0530

    added

--- addressbookcont/Continued.java
+++ addressbookcont/Continued.java
@@ -1,39 +1,52 @@
 package addressbookcont;
-import java.io.*;
-
-import java.util.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map.Entry;
-import java.util.Scanner;
-import java.util.stream.Collectors;
-
-public class Continued {
-	
-		.filter(map -> getCityname.equals(map.getValue()));
+.filter(map -> getCityname.equals(map.getValue()));
 System.out.println("Following persons belong to provided city:" + result);
 }
+
+// Count by State
 private static void CountbyState() {
 System.out.println("Enter the name of the state");
 String getStatename= stdlin.next();
 int count=Collections.frequency(deathNote.values(), getStatename);
+String getStatename = stdlin.next();
+int count = Collections.frequency(deathNote.values(), getStatename);
 System.out.println(count);
 
+
 }
+
+// Count by city
 private static void CountbyCity() {
 System.out.println("Enter the name of the city");
 String getCityname= stdlin.next();
 int count=Collections.frequency(deathNote.values(), getCityname);
+String getCityname = stdlin.next();
+int count = Collections.frequency(deathNote.values(), getCityname);
 System.out.println(count);
 
 }
+
+// Sort by first name
+private static void SortByFirstName(ArrayList<Info> friends) {
+Stream<Info> firstNameBasis =  friends.stream().sorted();
+System.out.println(firstNameBasis);
+
+}
+
 public static void main(String[] args) {
 
 Scanner stdlin = new Scanner(System.in);
- public static void main(String[] args) {
-	System.out.println("3. Delete an existing contact.");
-	System.out.println("4.Sort by City");
+@@ -159,7 +171,8 @@ public static void main(String[] args) {
+// initiating user functions of entries
+
+String user_input = "1";
+while ((user_input.equals("1") || user_input.equals("2") || user_input.equals("3"))) {
+while ((user_input.equals("1") || user_input.equals("2") || user_input.equals("3") || user_input.equals("4")
+		|| user_input.equals("5") || user_input.equals("6") || user_input.equals("7"))) {
+
+	// Checking in address list is present in hashmap
+	System.out.print("Enter the Name of the Address Book: ");
+@@ -182,7 +195,6 @@ public static void main(String[] args) {
 	System.out.println("5.Sort by State");
 	System.out.println("6.Count by State");
 	System.out.println("7.Sort by City");
@@ -41,17 +54,25 @@ Scanner stdlin = new Scanner(System.in);
 
 	user_input = stdlin.next();
 
+@@ -213,15 +225,19 @@ public static void main(String[] args) {
 	case "5":
 		SortCity();
 		break;
+		
 
 	case "6":
 		CountbyState();
 		break;
+		
 
 	case "7":
 		CountbyCity();
 		break;
 
+	case "8":
+		SortByFirstName(friends);
+		break;
+
 	default:
 		break;
+
